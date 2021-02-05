1f3674631e96049b1c1f7610517fb47aeba3353a
educalo77 <educalo@outlook.com>
2021-02-05 10:56:56 -0300

    sql details

--- backend/src/controllers/ratesControllers.js
+++ backend/src/controllers/ratesControllers.js
@@ -14,21 +14,6 @@ const createOne = (id_currency, value) => {
   });
 };
 
-// const getAll = (entrada) => {
-//   if (entrada === "btc") curId = 1;
-//   if (entrada === "eth") curId = 2;
-//   if (entrada === "ada") curId = 3;
-//   return new Promise((resolve, reject) => {
-//     Valor.findAll({
-//       where: { id_currency: curId },
-//       include: [Moneda],
-//     })
-//       .then((rates) => {
-//         resolve(rates[rates.length - 1]);
-//       })
-//       .catch((err) => reject(err));
-//   });
-// };
 const getAll = (entrada) => {
   const eNtrada = entrada.toUpperCase();
   return new Promise((resolve, reject) => {
